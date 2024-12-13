b790e45e564e491397a831139ee501f7112678e4
smallboyc <maxencedup44@gmail.com>
2024-12-13 11:18:37 +0100

    :sparkles: Steps component

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -1,43 +1,55 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import * as Progress from 'react-native-progress';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import * as Progress from "react-native-progress";
+import { Colors } from "@/constants/Colors";
+import { ThemedText } from "./ThemedText";
 
-export const Steps = () => {
-    const [progress, setProgress] = useState(1);
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textContainer: {
+        position: "absolute",
+        flexDirection: "row",
+        alignItems: "baseline",
+        gap: 2,
+    },
+    stepText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#00D8B6",
+    },
+    totalStepsText: {
+        fontSize: 18,
+        color: "#000",
+        fontWeight: "400",
+    },
+});
+
+
+export const Steps = ({ currentStep, totalSteps }: { currentStep: number, totalSteps: number }) => {
+
+    const progress = currentStep / totalSteps;
+    const COLORS = Colors.light
 
     return (
         <View style={styles.container}>
-            <View style={styles.progressContainer}>
-                <Progress.Circle
-                    size={120}
-                    progress={progress / 3}
-                    color="#76c7c0"
-                    thickness={10}
-                />
-                <Text style={styles.progressText}>{`${progress} / 3 `}</Text>
+            <Progress.Circle
+                size={55}
+                progress={progress}
+                showsText={false}
+                thickness={2}
+                color={COLORS.lightGreen}
+                unfilledColor={COLORS.White}
+                borderWidth={0}
+            />
+            <View style={styles.textContainer}>
+                <ThemedText variant="heading2" color="lightGreen">{currentStep}</ThemedText>
+                <ThemedText variant="heading2">/</ThemedText>
+                <ThemedText variant="heading2">{totalSteps}</ThemedText>
             </View>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    progressContainer: {
-        position: 'relative',
-        width: 120,
-        height: 120,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    progressText: {
-        position: 'absolute',
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#000',
-    },
-});
-
