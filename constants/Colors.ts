96ba7135f6db00b7941addbb6ed43fe77434cc75
smallboyc <maxencedup44@gmail.com>
2024-12-13 11:18:37 +0100

    :sparkles: Steps component

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,32 +1,32 @@
 export const Colors = {
-    light : {
-        Black: "#232A36",
+    light: {
         lightGreen: "#30F6B7",
         Green: "#3CC3AA",
         White: "#FFFFFF",
+        Black: "#232A36",
         Background: "#F5F6FA",
         Purple: "#6B31F5",
         Orange: "#F55931",
         Yellow: "#F5E731",
     },
 
-    dark : {
-        Black: "#232A36",
+    dark: {
         lightGreen: "#30F6B7",
         Green: "#3CC3AA",
         White: "#FFFFFF",
+        Black: "#232A36",
         Background: "#F5F6FA",
         Purple: "#6B31F5",
         Orange: "#F55931",
         Yellow: "#F5E731"
     },
 
-    gradient : {
-        greenGradient: {color1:"#30F6B7", color2:"#3CC3AA"},
-        scoreGradient20: {color1:"#30F6B7", color2:"#96F630"},
-        scoreGradient40: {color1:"#96F630", color2:"#F6E930"},
-        scoreGradient60: {color1:"#F6E930", color2:"#F69030"},
-        scoreGradient80: {color1:"#F69030", color2:"#F63030"},
-        scoreGradient100: {color1:"#F63030", color2:"#F63079"}
+    gradient: {
+        greenGradient: { color1: "#30F6B7", color2: "#3CC3AA" },
+        scoreGradient20: { color1: "#30F6B7", color2: "#96F630" },
+        scoreGradient40: { color1: "#96F630", color2: "#F6E930" },
+        scoreGradient60: { color1: "#F6E930", color2: "#F69030" },
+        scoreGradient80: { color1: "#F69030", color2: "#F63030" },
+        scoreGradient100: { color1: "#F63030", color2: "#F63079" }
     }
 }
