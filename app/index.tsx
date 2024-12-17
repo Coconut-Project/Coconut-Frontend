11fdcbe1f1ee2dbce16e240f42a300839c2ac631
Léo De Santis <leo.desantispro@gmail.com>
2024-12-17 16:49:25 +0100

    add Logo on Home

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,44 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
+import { ClassicButton } from "@/components/ClassicButton";
+import { LogoLightGreen } from "@/components/svg/logo/LogoLightGreen";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 20,
-        marginTop: 20,
-      }}
-    >
-      <ThemedText variant="display2">Welcome to Coconut !</ThemedText>
+    <View style={styles.container}>
+      <View style={styles.main}>
+      <LogoLightGreen/>
+      <View style={styles.section}>
+        <ThemedText variant="display2">Bienvenue sur Coconut !</ThemedText>
+        <ThemedText variant="body">L'éco-score textile à portée de main.</ThemedText>
+      </View>
+      </View>
+      <ClassicButton label="C'est parti !"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 40,
+  },
+  section: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  checkbox: {
+    margin: 8,
+    borderRadius: 5,
+    width: 25,
+    height: 25,
+  },
+
+  main:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center", 
+  }
+});
